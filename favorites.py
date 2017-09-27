7ba9091aedab4faa59e2f62b97d37522e1341799
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-27 13:51:09 -0400

    Updated from Brython Server: 9/27/2017 1:51:08 PM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#9/27/17
+#favorites.py - using a loop to repeat the user's entered favorite word their entered favorite number
 
+word = input('Enter your favorite word: ')
+num = int(input('Enter your favorite number: '))
+
+for i in range(0,num):
+    print(word)
