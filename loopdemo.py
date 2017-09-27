5305990a79f42a2ee296701af77ec8ab9678acd8
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-27 13:28:45 -0400

    Updated from Brython Server: 9/27/2017 1:28:44 PM

--- loopdemo.py
+++ loopdemo.py
@@ -1,1 +1,14 @@
+#Astro Ohnuma
+#9/27/17
+#loopdemo.py - how to use loops
+
+"""
+#print I love computer science 5 times
+for i in range(0,5):
+    print('I <3 Computer Science')"""
+
+#print the numbers from 1 to 20
+for i in range(1,21):
+    print(i)
+    
 
