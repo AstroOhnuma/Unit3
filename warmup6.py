782fc6bd6288e81dc85c2f9ad36d9437bfabb31d
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-28 14:22:07 -0400

    Updated from Brython Server: 9/28/2017 2:22:06 PM

--- warmup6.py
+++ warmup6.py
@@ -1,1 +1,10 @@
-
+#Astro Ohnuma
+#9/27/17
+#warmup6.py - counting to 10 mississipi using loops
+"""
+for i in range(1,11):
+    print(i,'mississipi')"""
+i = 1
+while i <=10:
+    print(i,'mississipi')
+    i += 1
