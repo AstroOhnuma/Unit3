7f1802239952f3023056264b59386813b30d7f36
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-29 14:29:17 -0400

    Updated from Brython Server: 9/29/2017 2:29:16 PM

--- warmup7.py
+++ warmup7.py
@@ -1,1 +1,12 @@
-
+#Astro Ohnuma
+#9/27/17
+#warmup7.py - finding the product of numbers from 1 to 20 with loops
+"""
+for i in range(1,21):
+    print(i)"""
+total = 1
+i = 1
+while i <=20:
+    total *= i
+    i += 1
+print(total)
