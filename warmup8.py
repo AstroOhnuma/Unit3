26f3738e60f6fbdfd70c8db730b648832e7c85ec
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-02 14:36:39 -0400

    Updated from Brython Server: 10/2/2017 2:36:38 PM

--- warmup8.py
+++ warmup8.py
@@ -1,6 +1,8 @@
 #Astro Ohnuma
 #10/2/17
-#warmup8.py - Find the sum of all positive integers less than 100,000 that are divisble by 3, 10, and 2017
-
+#warmup8.py - Find the sum of all positive integers less than 100,000 that are divisble by 3, 10, and 17
+total = 0
 for i in range(1,100001):
-    print(i//3)
+    total+= i
+    if i%3==0 and i%10==0 and i%17==0:
+        print(i)
