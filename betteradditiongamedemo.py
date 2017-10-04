215f45a57c8b5b9dbfd67b3c3806716eaf45f35a
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-04 13:38:29 -0400

    Updated from Brython Server: 10/4/2017 1:38:28 PM

--- betteradditiongamedemo.py
+++ betteradditiongamedemo.py
@@ -8,6 +8,22 @@ numcorrect = 0
 while numcorrect <5:
     num1 = randint(-10,10)
     num2 = randint(-10,10)
-    question = 'What is'+str(num1)+'+'+str(num2)+'?'
+    question = 'What is '+str(num1)+'+'+str(num2)+'? '
     answer = int(input(question))
-    break
+    if num1+num2 == answer:
+        print('Correct!')
+        numcorrect += 1
+    else:
+        print('The answer was', num1+num2)
+        
+print('Congratulations! You win!')
+
+
+
+
+
+
+
+
+
+
