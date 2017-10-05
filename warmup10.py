89baa2cb0f0338de122c9a5e17c9262b5a900fac
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-05 14:43:40 -0400

    Updated from Brython Server: 10/5/2017 2:43:40 PM

--- warmup10.py
+++ warmup10.py
@@ -1,1 +1,16 @@
+#Astro Ohnuma
+#10/5/17
+#warmup10.py - making wallpaper
+
+from ggame import *
+
+red = Color(0xFF0000,1)
+
+dot = CircleAsset(20,LineStyle(1,red),red)
+triangle = PolygonAsset([(0,0), (30,10), (40,20)],LineStyle(1,red),red)
+
+for j in range(15):#prints the row 10 times
+    for i in range(20):#prints one row of dots
+        Sprite(dot,(20 + 50*i,20 + 50*j))
+App().run()
 
