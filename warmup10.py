b48b7c1b56cfcc320c490b860d292b699e9c582d
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-05 14:44:33 -0400

    Updated from Brython Server: 10/5/2017 2:44:33 PM

--- warmup10.py
+++ warmup10.py
@@ -7,10 +7,11 @@ from ggame import *
 red = Color(0xFF0000,1)
 
 dot = CircleAsset(20,LineStyle(1,red),red)
-triangle = PolygonAsset([(0,0), (30,10), (40,20)],LineStyle(1,red),red)
+triangle = PolygonAsset([(20,20), (30,10), (40,20)],LineStyle(1,red),red)
 
 for j in range(15):#prints the row 10 times
     for i in range(20):#prints one row of dots
         Sprite(dot,(20 + 50*i,20 + 50*j))
+Sprite(triangle)
 App().run()
 
