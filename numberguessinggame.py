0e36e7adf8b91729cd7433a7abff3c01401a82ef
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-12 14:54:25 -0400

    Updated from Brython Server: 10/12/2017 2:54:24 PM

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -1,5 +1,10 @@
 #Astro Ohnuma
 #10/4/17
 #numbergeussinggame.py - geussing a number between 1 and 100
-
-
+from random import randint
+random = randint(1,100)
+while True:
+    num = int(input('Guess a number between 1 and 100: ')
+    if num < random:
+        print('Too low!')
+    
