21c40f317811c56f8b3d93f6b3471ab9ed5e2fae
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-13 14:31:56 -0400

    Updated from Brython Server: 10/13/2017 2:31:55 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,21 @@
+#Astro Ohnuma
+#10/13/17
+#quiz3.py - Unit 3 quiz!
 
+for i in range(-5,6):
+    print(i)
+n = 18
+
+while n<33:
+    print(n)
+    n = n+2
+    
+total = 0
+for i in range(13,332,2):
+    total += i
+print(total)
+
+while True:
+    word = input('Enter a word: ')
+    if 'z' in word:
+        break
